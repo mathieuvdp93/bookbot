89178f537999909cd9bb5888373a9a636b6980cc
Mathieu Vandeputte <mathieuvandeputte93@gmail.com>
2026-01-03 21:22:27 +0100

    updated main and stats to count chars

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import get_book_text
+from stats import get_book_text, get_char_amount
 
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    words = text.split()
-    word_count = len(words)
-    print(f"Found {word_count} total words")
+    #words = text.split()
+    #word_count = len(words)
+    #print(f"Found {word_count} total words")
+    char_count = get_char_amount(text)
+    print(char_count)
+
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def get_book_text(fp):
     with open(fp) as f:
         return f.read()
+
+def get_char_amount(text):
+    result = {}
+
+    text = text.lower()
+
+    for char in text:
+        if char in result:
+            result[char] += 1
+        else:
+            result[char] = 1
+
+    return result
